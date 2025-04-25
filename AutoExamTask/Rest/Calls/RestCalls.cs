fabbc05ec4d202f1ccde496c96042c3bf9bbffcf
Boris Popov <borisbpopov@gmail.com>
2025-04-25 19:07:43 +0300

    Initial skelleton set up

---
+++ AutoExamTask/Rest/Calls/RestCalls.cs
@@ -0,0 +1,30 @@
+﻿using RestSharp;
+
+namespace AutoExamTask.Rest.Calls
+{
+    public class RestCalls
+    {
+        public RestResponse LoginCall(string url, string username, string password, bool rememberMe = false)
+        {
+            RestClientOptions options = new RestClientOptions(url)
+            {
+                Timeout = TimeSpan.FromSeconds(120),
+            };
+
+            RestClient client = new RestClient(options);
+
+            RestRequest request = new RestRequest("/users/login", Method.Post);
+
+            request.AddHeader("Content-Type", "application/json");
+
+            string body = @"{""usernameOrEmail"":""" + username + @""",""password"":""" + password + @""",""rememberMe"":""" + rememberMe.ToString().ToLower() + @"""}";
+
+            request.AddStringBody(body, DataFormat.Json);
+
+            RestResponse response = client.Execute(request);
+
+            return response;
+        }
+
+    }
+}
---
+++ AutoExamTask/Utilities/UtilitiesMethods.cs
@@ -0,0 +1,77 @@
+﻿using AventStack.ExtentReports;
+using NUnit.Framework;
+using Reqnroll;
+
+namespace AutoExamTask.Utilities
+{
+    public enum LogStatuses
+    {
+        Info,
+        Warning,
+        Debug
+    }
+    public class UtilitiesMethods
+    {
+        private static Random random = new Random();
+        public static void AssertEqual<T>(T expected, T actual, string message, ScenarioContext scenarioContext)
+        {
+            ExtentTest _test = scenarioContext.Get<ExtentTest>("ExtentTest");
+
+            if (!EqualityComparer<T>.Default.Equals(expected, actual))
+            {
+                Logger.Log.Error(message + Environment.NewLine + $"Expected: {expected}, but was: {actual}");
+                _test.Log(Status.Fail, message + Environment.NewLine + $"Expected: {expected}, but was: {actual}");
+                Assert.Fail(message + Environment.NewLine + $"Expected: {expected}, but was: {actual}");
+            }
+        }
+
+        /// <summary>
+        /// Log message into the report log and test run log file
+        /// </summary>
+        /// <param name="message">Message to be logged</param>
+        /// <param name="scenarioContext">Scenario context</param>
+        /// <param name="status">Type of log message, e.g. Info, Debug</param>
+        public static void LogMessage(
+            string message,
+            ScenarioContext scenarioContext,
+            LogStatuses status = LogStatuses.Info)
+        {
+            ExtentTest _test = scenarioContext.Get<ExtentTest>("ExtentTest");
+            if (status == LogStatuses.Info)
+            {
+                _test.Log(Status.Info, message);
+                Logger.Log.Info(message);
+            }
+            else if (status == LogStatuses.Warning)
+            {
+                _test.Log(Status.Warning, message);
+                Logger.Log.Warn(message);
+            }
+            else if (status == LogStatuses.Debug)
+            {
+                _test.Log(Status.Info, message);
+                Logger.Log.Debug(message);
+            }
+        }
+        public static string GenerateOrderNumber()
+        {
+            string prefix = "ORD";
+            string datePart = DateTime.Now.ToString("yyyyMMdd");
+            string randomPart = GenerateRandomString(6);
+            return $"{prefix}-{datePart}-{randomPart}";
+        }
+        private static string GenerateRandomString(int length)
+        {
+            const string chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
+            char[] result = new char[length];
+
+            for (int i = 0; i < length; i++)
+            {
+                result[i] = chars[random.Next(chars.Length)];
+            }
+
+            return new string(result);
+        }
+    }
+        
+}
